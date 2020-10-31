e82cf497d3b4789420267f6f2ce3ef932f06a7bf
TomCorson <corsontom@yahoo.com>
2020-10-31 11:16:17 -0400

    added comments

--- src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
@@ -9,26 +9,15 @@ import java.util.Scanner;
  */
 public class Console {
 
-    public static void print(String output, Object... args) {
-        System.out.printf(output, args);
+    public static void main(String[] args) {
+        //print current number
+        //print menu
+
+        //ask input for a math utility selection
+        //call appropriate math function
+
+        //input numbers as argument(s)
+        //return updated number to console
     }
 
-    public static void println(String output, Object... args) {
-        print(output + "\n", args);
-    }
-
-    public static String getStringInput(String prompt) {
-        Scanner scanner = new Scanner(System.in);
-        println(prompt);
-        String userInput = scanner.nextLine();
-        return userInput;
-    }
-
-    public static Integer getIntegerInput(String prompt) {
-        return null;
-    }
-
-    public static Double getDoubleInput(String prompt) {
-        return null;
-    }
 }
