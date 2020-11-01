772cd018462af261ca458690035b7b4f26823559
hillary <hillary@new-host-4.home>
2020-11-01 11:59:19 -0500

    hillCoreBasicsCalc2

--- src/main/java/Core.java
+++ src/main/java/Core.java
@@ -2,68 +2,98 @@ import java.util.Scanner;
 
 public class Core {
 
-    double num1;
-    double num2;
-    char operator;
-    int exponent;
+    public double num1;
+    public double num2;
+    public char operator;
+    public int exponent;
+    public double output;
 
     Scanner chdScan = new Scanner(System.in);
 
-    public static void Simple(Strings[] args) {
+// default state... is this how to do it?
+
+    System.out.println("0");
+
+/* method for + - * /
+with ERR message for division by 0 and wrong operator*/
+
+    public void Simple(Strings[] args) {
         System.out.println("Enter first number: ");
         num1 = chdScan.nextDouble();
         else(System.out.println("Enter second number: "));
         num2 = chdScan.nextDouble();
         System.out.println("Choose operator: ");
         operator = chdScan.next.charAt(0);
+
+        switch (operator) {
+            case '+':
+                output = num1 + num2;
+                break;
+            case '-':
+                output = num1 - num2;
+                break;
+            case '*':
+                output = num1 * num2;
+                break;
+            case '/':
+                output = num1 / num2;
+                break;
+            default:
+                System.out.printf("ERR: wrong operator");
+        }
+        if (output = num1 / 0) {
+            System.out.println("ERR");
+        } else {
+            System.out.println(output);
+        }
     }
 
-    double output;
+// method for finding square root
 
-        switch(operator)
+    public void SquareRoot(Strings[] args) {
 
-    {
-        case '+':
-            output = num1 + num2;
-            break;
-        case '-':
-            output = num1 - num2;
-            break;
-        case '*':
-            output = num1 * num2;
-            break;
-        case '/':
-            output = num1 / num2;
-            break;
-        default:
-            System.out.printf("ERR: wrong operator");
-    }
-        if(output =num1 /0)
-
-    {
-        System.out.println("ERR");
-    }
-        else
-
-    {
-        System.out.println(output);
-    }
-
-    public static void Square(Strings[] args) {
         System.out.println("Enter first number: ");
         num1 = chdScan.nextDouble();
         System.out.println("Choose operator: ");
         operator = chdScan.next.charAt(0);
-        System.out.println(Math.sqrt(num1));
+        output = Math.sqrt(num1);
+
+        System.out.println(output);
     }
 
-    public static void Exp(Strings[] args) {
+// method for exponents
+
+    public void Square(Strings[] args) {
+
         System.out.println("Enter first number: ");
         num1 = chdScan.nextDouble();
         System.out.println("Choose operator: ");
         operator = chdScan.next.charAt(0);
         System.out.println("Enter exponent: ");
         exponent = chdScan.next.int();
-        System.out.println(Math.pow(num1, x));
+        output = Math.pow(num1, x);
+
+        System.out.println(output);
     }
-}
+
+//method for changing number to negative or positive
+
+    public void NegPos(Strings[] args) {
+        double change = output *= -1;
+        System.out.println(output);
+    }
+
+//method for inverting a number
+
+     public void Inverse(Strings[] args){
+        double inversion = 1 / output;
+         System.out.println(output);
+     }
+
+//method for clearing display for general use, must use if Error
+     public void Clear(Strings[] args){
+        int reset = 0;
+        System.out.println(reset);
+      }
+    }
+
