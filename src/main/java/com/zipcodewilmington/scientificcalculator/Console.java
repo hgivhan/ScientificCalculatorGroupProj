f32463b192822d5574234894f4ee1acc62a0fa63
deon <deon@zipcodes-MacBook-Pro-23.local>
2020-11-01 14:43:10 -0500

    Menu

--- src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
@@ -1,23 +1,76 @@
 package com.zipcodewilmington.scientificcalculator;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- * TOM WROTE HERE!!!!!!!!!!!!
- * !!!!!!!!!!!!
- * Created by leon on 2/9/18.
- */
+
 public class Console {
 
+    public static void print(String output, Object... args) {
+        System.out.println(output, args);
+    }
+
+    public static void println(String output, Object... args) {
+        print(output + "\n", args);
+
+    }
+
+    public static void startConsole(){
+        Scanner scan = new Scanner(System.in);
+        boolean keepOn = true;
+        System.out.println("Welcome to CHD's Calculator");
+
+        while (keepOn) {
+            Console.print("\n" +
+                    "Choose from menu:\n" +
+                    "Type 1 - Basic Calculator: \n" +
+                    "Type 2 - Scientific Calculator: \n" +
+                    "Type 3 - Memory: \n" +
+                    "Type 4 - Quit \n" +
+                    "Your work = " + "\n" +
+                    "Enter choice here: ");
+
+            try {
+                int input = scan.nextInt();
+                switch (input) {
+                    case 1:
+                        // BasicCalculator.basicCalculatorOptions();
+                        break;
+                    case 2:
+                        // ScientificCalculator.scientificCalculatorOptions();
+                        break;
+                    case 3:
+                        // System.out.println(currentMemory);
+                        break;
+                    case 4:
+                        keepOn = false;
+                        System.out.println("Turning off!");
+                }
+            } catch (InputMismatchException e) {
+                scan.next();
+                System.out.println("\n" + "Please choose option that's on the menu bruh!");
+            }
+
+        }
+        }
+
+    }
+
     public static void main(String[] args) {
-        //print current number
-        //print menu
 
-        //ask input for a math utility selection
-        //call appropriate math function
+    
 
-        //input numbers as argument(s)
-        //return updated number to console
+
+        /* print current number
+        print menu
+
+        ask input for a math utility selection
+        call appropriate math function
+
+        input numbers as argument(s)
+        return updated number to console
     }
 
 }
+
+         */
